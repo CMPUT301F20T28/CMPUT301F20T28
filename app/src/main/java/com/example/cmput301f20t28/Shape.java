f8de5e9e56da1ae9e078c6d25f6157fa0166920a
shanzhizhang <shanzhi1@ualberta.ca>
2020-10-01 17:17:11 -0600

    adding shape function

---
+++ app/src/main/java/com/example/cmput301f20t28/Shape.java
@@ -0,0 +1,27 @@
+package com.example.cmput301f20t28;
+
+public abstract class Shape {
+    private int X;
+    private int y;
+
+    public Shape(int x, int y) {
+        X = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        X = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
