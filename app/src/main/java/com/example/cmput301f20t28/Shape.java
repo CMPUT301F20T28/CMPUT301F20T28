e2aa8ab4b9166d17e46dd30de565c02087997e1e
zhininghjl <zhininghjl@gmail.com>
2020-10-03 16:05:24 -0600

    update shape

--- app/src/main/java/com/example/cmput301f20t28/Shape.java
+++ app/src/main/java/com/example/cmput301f20t28/Shape.java
@@ -3,7 +3,8 @@ package com.example.cmput301f20t28;
 public abstract class Shape {
     private int x;
     private int y;
-   
+    String color = "blue";
+
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
